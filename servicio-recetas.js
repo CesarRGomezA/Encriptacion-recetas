98aa76b0e07a4a3f5a728e76cdec2b9d39205141
CesarRGomezA <CesarRGomezA@gmail.com>
2021-02-09 16:51:04 -0700

    segundo push

--- servicio-recetas.js
+++ servicio-recetas.js
@@ -1,6 +1,11 @@
 const { request } = require("https");
-
-const server = require("fastify")();
+const fs = require("fs");
+const server = require("fastify")({
+    https : {
+        key: fs.readFileSync(__dirname + "/tls/llave-privada.key"),
+        cert: fs.readFileSync(__dirname + "/shared/tls/certificado-publico.cert")
+    }
+});
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 4000;
@@ -8,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 console.log(`Proceso pid =${process.pid}`);
 
 server.get('/recetas/:id', async (request, response) => {
-    console.log(`Proceso de atencion de dolicitud pid=${process.pid}`);
+    console.log(`Proceso de atencion de solicitud pid=${process.pid}`);
     const id = Number(request.params.id);
     if (id !== 42) {
         response.statusCode = 404;
@@ -19,7 +24,7 @@ server.get('/recetas/:id', async (request, response) => {
         receta : {
             id,
             nombre: "Tacos de pollo",
-            pasos: "Agarras la tortlla y le pones pollo",
+            pasos: "Agarras la tortilla y le pones pollo",
             ingradientes: [
                 {id: 1, nombre: "Tortilla", cantidad: "2 unidades"},
                 {id: 2, nombre: "Pollo", cantidad: "80 grs"}
